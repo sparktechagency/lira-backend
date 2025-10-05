1c03bbceb929c844d04e811fcd3007d000403cbf
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-10-05 11:10:45 +0600

    feat(community): add community vote creation functionality
    implement createCommunityVote service and controller methods to handle community vote creation

--- src/app/modules/community/community.controller.ts
+++ src/app/modules/community/community.controller.ts
@@ -1,3 +1,19 @@
-export const CommunityController = {
+import { StatusCodes } from "http-status-codes";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { CommunityService } from "./community.service";
 
+
+const createCommunityVote = catchAsync(async (req, res) => {
+    const { id } = req.user as { id: string };
+    const result = await CommunityService.createCommunityVote(id, req.body);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Community vote created successfully',
+        data: result,
+    });
+});
+export const CommunityController = {
+    createCommunityVote,
 }
--- src/app/modules/community/community.service.ts
+++ src/app/modules/community/community.service.ts
@@ -1,3 +1,17 @@
-export const CommunityService = {
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../../errors/AppError";
+import { ICommunityVote } from "./community.interface";
+import { CommunityVoteModel } from "./community.model";
 
+const createCommunityVote = async (userId: string, payload: ICommunityVote) => {
+    const data = { ...payload, userId };
+    const result = await CommunityVoteModel.create(data);
+    if (!result) {
+        throw new AppError(StatusCodes.BAD_REQUEST, 'Failed to create community vote');
+    }
+    return result;
+}
+
+export const CommunityService = {
+    createCommunityVote,
 }

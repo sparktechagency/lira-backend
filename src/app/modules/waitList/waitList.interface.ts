1a1ec81531f527ac517d840c1ce3b60375b8d01a
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-11-04 12:19:27 +0600

    feat(waitList): add initial waitList module files
    Add service, controller, route, interface and model files for waitList module

---
+++ src/app/modules/waitList/waitList.interface.ts
@@ -0,0 +1,6 @@
+import { Types } from "mongoose";
+
+export interface IWaitList extends Document {
+    userId: Types.ObjectId;
+    contestId: Types.ObjectId;
+}
---
+++ src/app/modules/waitList/waitList.model.ts
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+import { IWaitList } from "./waitList.interface";
+
+const WaitListSchema = new Schema<IWaitList>({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    contestId: { type: Schema.Types.ObjectId, ref: 'Contest', required: true },
+}, {
+    timestamps: true,
+});
+
+export const WaitListModel = model<IWaitList>('WaitList', WaitListSchema);

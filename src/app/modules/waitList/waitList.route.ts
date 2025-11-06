f7e8216b5be168ee84b213be7a63781265c3062b
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-11-06 16:42:18 +0600

    refactor(waitList): remove extra blank lines in route file

--- src/app/modules/waitList/waitList.route.ts
+++ src/app/modules/waitList/waitList.route.ts
@@ -3,10 +3,7 @@ import auth from '../../middleware/auth';
 import { USER_ROLES } from '../../../enums/user';
 import { WaitListController } from './waitList.controller';
 
-
-
 const router = express.Router();
-
 router.post('/create', auth(USER_ROLES.USER), WaitListController.createWaitList);
 router.get('/', auth(USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN), WaitListController.getAllWaitList);
 router.get('/:id', auth(USER_ROLES.ADMIN, USER_ROLES.SUPER_ADMIN), WaitListController.getSingleWaitList);

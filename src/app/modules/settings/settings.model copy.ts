f6df0a0399a7e7135637c00fdb245d71805b7031
rakiburrahman307 <rakiburrahman307@gmail.com>
2025-11-10 19:14:12 +0600

    feat(settings): add mongoose model for application settings

---
+++ src/app/modules/settings/settings.model copy.ts
@@ -0,0 +1,55 @@
+import { Schema, model } from 'mongoose';
+import { ISettings } from './settings.interface';
+
+const settingsSchema = new Schema<ISettings>(
+     {
+          privacyPolicy: {
+               type: String,
+               default: '',
+          },
+          aboutUs: {
+               type: String,
+               default: '',
+          },
+          support: {
+               type: String,
+               default: '',
+          },
+          termsOfService: {
+               type: String,
+               default: '',
+          },
+          social: {
+               contactEmail: {
+                    type: String,
+                    default: '',
+               },
+               phone: {
+                    type: String,
+                    default: '',
+               },
+               address: {
+                    type: String,
+                    default: '',
+               },
+               instagram: {
+                    type: String,
+                    default: '',
+               },
+               linkedin: {
+                    type: String,
+                    default: '',
+               }
+          },
+          note: {
+               type: String,
+               default: '',
+          }
+     },
+     { timestamps: true },
+);
+
+// Create the model
+const Settings = model<ISettings>('Settings', settingsSchema);
+
+export default Settings;
